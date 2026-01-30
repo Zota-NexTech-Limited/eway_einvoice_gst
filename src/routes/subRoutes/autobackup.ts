857e6c634882b78d227f748720a15c31cd0c78fe
chethan kumar m s <chethan@zotanextech.com>
2026-01-30 18:41:50 +0530

    backup apis added in routes

--- src/routes/subRoutes/autobackup.ts
+++ src/routes/subRoutes/autobackup.ts
@@ -283,3 +283,266 @@ export function scheduleBackupJobs() {
 //         }
 //     });
 // });
+
+// import { spawn } from "child_process";
+// const s3Two = new S3Client({
+//     region: "us-east-1",  // Synology requires ANY region (use us-east-1)
+//     endpoint: "https://in-maa-1.linodeobjects.com",
+//     forcePathStyle: true,
+//     credentials: {
+//         accessKeyId: "RCVP30CK9S7VI1ILIYOC",
+//         secretAccessKey: "tkwmDPuC2q81S8ywa9W35D3Vy3dmBiPaQ125komR",
+//     }
+// });
+
+// function formatFolderName(name: string) {
+//     return name
+//         .toLowerCase()
+//         .trim()
+//         .replace(/\s+/g, "-")          // spaces → hyphen
+//         .replace(/[^a-z0-9-_]/g, "")   // remove special chars
+//         .replace(/-+/g, "-");          // remove duplicate hyphens
+// }
+
+// userRoutes.post("/api/create-root-folder", async (req, res) => {
+//     try {
+//         const { folderName } = req.body;
+
+//         if (!folderName) {
+//             return res.status(400).send({
+//                 status: false,
+//                 message: "folderName is required"
+//             });
+//         }
+
+//         const cleanName = formatFolderName(folderName);
+//         const folderKey = `${cleanName}/`;
+
+//         await s3Two.send(new PutObjectCommand({
+//             Bucket: "db-backup",
+//             Key: folderKey,
+//             Body: ""
+//         }));
+
+//         if (folderKey) {
+
+//             const folderKey = `archive-${cleanName}/`;
+
+//             await s3Two.send(new PutObjectCommand({
+//                 Bucket: "db-backup",
+//                 Key: folderKey,
+//                 Body: ""
+//             }));
+
+//         }
+//         res.send({
+//             status: true,
+//             message: "Folder created successfully",
+//             folder: folderKey
+//         });
+
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({
+//             status: false,
+//             message: "Folder creation failed"
+//         });
+//     }
+// });
+
+// userRoutes.get("/api/get-root-folders", async (req, res) => {
+//     const { bucket_name } = req.query;
+
+//     try {
+//         const result = await s3Two.send(new ListObjectsV2Command({
+//             Bucket: `${bucket_name}`,
+//             Delimiter: "/"
+//         }));
+
+
+//         const folders =
+//             result.CommonPrefixes?.map(p =>
+//                 p.Prefix?.replace("/", "")
+//             ) || [];
+
+//         res.send({
+//             status: true,
+//             folders
+//         });
+
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({
+//             status: false,
+//             message: "Failed to fetch folders"
+//         });
+//     }
+// });
+// import { withClient } from '../../util/database';
+
+// // userRoutes.post("/api/create-folder-sutomatically", async (req: any, res) => {
+// //     return withClient(async (client: any) => {
+// //         try {
+
+// //             const storeResult = await client.query(`
+// //     SELECT store_id, company
+// //     FROM store_information_for_user_table
+// //     LIMIT 1
+// // `);
+
+// //             if (storeResult.rows[0]) {
+// //                 const { store_id, company } = storeResult.rows[0];
+
+// //                 const folderName = `${store_id} ${company}`
+
+// //                 if (folderName) {
+// //                     const cleanName = formatFolderName(folderName);
+// //                     const folderKey = `${cleanName}/`;
+
+// //                     /* ---------- create folder ---------- */
+// //                     await s3Two.send(
+// //                         new PutObjectCommand({
+// //                             Bucket: 'db-backup',
+// //                             Key: folderKey,
+// //                             Body: ""
+// //                         })
+// //                     );
+
+
+// //                     const folderKeyTwo = `archive-${cleanName}/`;
+
+// //                     await s3Two.send(new PutObjectCommand({
+// //                         Bucket: "db-backup",
+// //                         Key: folderKeyTwo,
+// //                         Body: ""
+// //                     }));
+// //                 }
+// //             }
+
+
+
+// //             res.send({
+// //                 status: true,
+// //                 message: "Folder created successfully",
+// //                 folder: null
+// //             });
+
+// //         } catch (error) {
+// //             console.error("Folder create error:", error);
+// //             res.status(500).send({
+// //                 status: false,
+// //                 message: "Failed to create folder"
+// //             });
+// //         }
+// //     });
+// // });
+
+// userRoutes.post("/api/create-folder-sutomatically", async (req: any, res) => {
+//     return withClient(async (client: any) => {
+//         try {
+
+//             /* ---------- fetch store ---------- */
+//             // const storeResult = await client.query(`
+//             //     SELECT store_id, company
+//             //     FROM store_information_for_user_table
+//             //     LIMIT 1
+//             // `);
+
+//             // if (!storeResult.rows[0]) {
+//             //     return res.send({
+//             //         status: false,
+//             //         message: "Store not found"
+//             //     });
+//             // }
+
+//             // const { store_id, company } = storeResult.rows[0];
+
+//             const store_id = "STR002"
+//             const company = "Zota Nextech"
+//             const folderName = `${store_id} ${company}`;
+//             const cleanName = formatFolderName(folderName);
+
+//             /* ---------- create folders ---------- */
+//             await s3Two.send(new PutObjectCommand({
+//                 Bucket: "db-backup",
+//                 Key: `${cleanName}/`,
+//                 Body: ""
+//             }));
+
+//             await s3Two.send(new PutObjectCommand({
+//                 Bucket: "db-backup",
+//                 Key: `archive-${cleanName}/`,
+//                 Body: ""
+//             }));
+
+//             /* ---------- dump filename ---------- */
+//             const dateStr = new Date().toISOString().split("T")[0];
+//             const dumpKey = `${cleanName}/backup_${dateStr}.dump`;
+
+//              /* ---------- pg_dump ---------- */
+//             const pgDumpPath = "C:\\Program Files\\PostgreSQL\\17\\bin\\pg_dump.exe"; // use system PATH
+
+//             const args: string[] = [
+//                 "-h", '139.162.161.40',
+//                 "-p", '5432',
+//                 "-U", 'envguideuser',
+//                 "-Fc",
+//                 'envguide'
+//             ];
+
+//             console.log("Starting DB backup...");
+
+//             const dump = spawn(pgDumpPath, args, {
+//                 env: {
+//                     ...process.env,
+//                     PGPASSWORD: '1234567890'
+//                 }
+//             });
+
+//             const chunks: Buffer[] = [];
+
+//             dump.stdout.on("data", chunk => {
+//                 chunks.push(chunk);
+//             });
+
+//             dump.stderr.on("data", data => {
+//                 console.error("pg_dump error:", data.toString());
+//             });
+
+//             dump.on("close", async code => {
+//                 if (code !== 0) {
+//                     return res.send({
+//                         status: false,
+//                         message: "Backup failed"
+//                     });
+//                 }
+
+//                 const buffer = Buffer.concat(chunks);
+
+//                 await s3Two.send(new PutObjectCommand({
+//                     Bucket: "db-backup",
+//                     Key: dumpKey,
+//                     Body: buffer,
+//                     ContentType: "application/octet-stream",
+//                     ContentLength: buffer.length
+//                 }));
+
+//                 console.log("Backup uploaded");
+
+//                 res.send({
+//                     status: true,
+//                     message: "Backup uploaded",
+//                     file: dumpKey
+//                 });
+//             });
+       
+
+//         } catch (error) {
+//             console.error("Backup error:", error);
+//             res.status(500).send({
+//                 status: false,
+//                 message: "Backup failed"
+//             });
+//         }
+//     });
+// });
